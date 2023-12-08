9d8837cf0142e2e187acebdd1f45897e32ea8296
Andrew Lam <Andrew@operationshero.com>
2023-12-08 12:38:04 -0500

    first commit

---
+++ paroject4Code.py
@@ -0,0 +1,13 @@
+###########################################
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# Load data into frame
+df = pd.read_csv('DS3_weapon.csv')
+
+
+
+
